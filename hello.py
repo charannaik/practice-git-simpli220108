c6b4bbafeaadc4fe5f51098961e51dde19f52597
charannaik <charannaik@users.noreply.github.com>
2022-02-03 00:20:09 +0530

    Updated on github UI

--- hello.py
+++ hello.py
@@ -3,3 +3,8 @@ print {'Hello All again'}
 print {'Hello All again and again'}
 print {'Hello All again 4 times'}
 print {'Hello All again 5 times'}
+a=10
+print(a)
+b=20
+print(b)
+print('Addition is' a+b)
